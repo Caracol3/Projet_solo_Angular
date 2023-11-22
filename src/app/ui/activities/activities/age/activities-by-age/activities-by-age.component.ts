f9d66f41d3ab502c2090e5d7fb0862baf88bd4fc
Mathieu Coulibaly <mathieu.coulibaly@reseau.sncf.fr>
2023-11-22 22:20:45 +0100

    searchbar almost ok

--- src/app/ui/activities/activities/age/activities-by-age/activities-by-age.component.ts
+++ src/app/ui/activities/activities/age/activities-by-age/activities-by-age.component.ts
@@ -5,14 +5,13 @@ import { Activity } from 'src/app/models/activity.model';
 @Component({
   selector: 'app-activities-by-age',
   templateUrl: './activities-by-age.component.html',
-  styleUrls: ['./activities-by-age.component.scss']
+  styleUrls: ['./activities-by-age.component.scss'],
 })
-export class ActivitiesByAgeComponent implements OnInit{
-
-activities: Activity[] = [];
+export class ActivitiesByAgeComponent implements OnInit {
+  activities: Activity[] = [];
   allActivities: Activity[] = [];
 
-  constructor(private activitiesService: ActivitiesService) { }
+  constructor(private activitiesService: ActivitiesService) {}
 
   ngOnInit(): void {
     this.loadAllActivities();
@@ -31,13 +30,29 @@ activities: Activity[] = [];
   }
 
   filterByAge(age: number): void {
-    this.activities = this.allActivities.filter(activity => activity.recommendedAge === age);
+    this.activities = this.allActivities.filter(
+      (activity) => activity.recommendedAge === age
+    );
+  }
+
+  searchActivities(searchTerm: string): void {
+    if (!searchTerm) {
+      this.activities = this.allActivities;
+    } else {
+      this.activities = this.allActivities.filter(
+        (activity) =>
+          activity.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          activity.description.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
   }
 
   filterActivities(ageValue: string): void {
     const age = Number(ageValue);
     if (!isNaN(age)) {
-      this.activities = this.allActivities.filter(activity => activity.recommendedAge === age);
+      this.activities = this.allActivities.filter(
+        (activity) => activity.recommendedAge === age
+      );
     }
   }
 }
